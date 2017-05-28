5904b59e1ec98f77fb8d85fba4f502de46e9e9db
Arifur Rahman <ar883@scarletmail.rutgers.edu>
2017-05-28 05:36:13 -0400

    Calculate mean of four user inputted integers

---
+++ AverageCalc/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3, number4;
+
+            Console.Write("Enter the First number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the Second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the third number: ");
+            number3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the four number: ");
+            number4 = Convert.ToInt32(Console.ReadLine());
+
+            int result = (number1 + number2 + number3 + number4) / 4;
+            Console.WriteLine("The average of {0} , {1} , {2} , {3} is: {4} ",
+            number1, number2, number3, number4, result);
+        }
+    }
+}
